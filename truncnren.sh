e768c09b063c949721642017a639ae2c887ad268
Ubuntu <ubuntu@ip-172-31-18-240.us-west-2.compute.internal>
2016-11-11 14:09:13 +0000

    Adding truncnren.sh

--- truncnren.sh
+++ truncnren.sh
@@ -7,12 +7,14 @@ if [ "$#" -ne 1 ] || [ ! -d $1 ] ; then
 fi
 
 OSADADIR=$1
+DIRTREE="dir1/dir2/dir3"
+FILE="file-40900.txt"
 
 echo "Usando $OSADADIR..."
 
-mkdir -p dir1/dir2/dir3
-truncate -s 40900 dir1/dir2/dir3/file-40900.txt
-truncate -s 1000 dir1/dir2/dir3/file-40900.txt
-mv dir1 mydir
-stat dir1/dir2/dir3/file-40900.txt
+mkdir -p $OSADADIR/$DIRTREE
+truncate -s 40900 $OSADADIR/$DIRTREE/$FILE
+truncate -s 1000 $OSADADIR/$DIRTREE/$FILE
+mv $OSADADIR/dir1 $OSADADIR/mydir
+stat $OSADADIR/mydir/dir2/dir3/$FILE
 
