b2f4cac98eec85ac03562aad370196e7a22f2040
Ubuntu <ubuntu@ip-172-31-18-240.us-west-2.compute.internal>
2016-11-11 14:20:06 +0000

    Fixed consistency.sh to use md5sum instead of md5 (mac osx) Added file rename to truncnren.sh

--- truncnren.sh
+++ truncnren.sh
@@ -8,13 +8,17 @@ fi
 
 OSADADIR=$1
 DIRTREE="dir1/dir2/dir3"
+NEWDIRTREE="mydir/dir2/dir3"
+
 FILE="file-40900.txt"
+NEWFILE="file-1000.txt"
 
 echo "Usando $OSADADIR..."
 
 mkdir -p $OSADADIR/$DIRTREE
 truncate -s 40900 $OSADADIR/$DIRTREE/$FILE
 truncate -s 1000 $OSADADIR/$DIRTREE/$FILE
+mv $OSADADIR/$DIRTREE/$FILE $OSADADIR/$DIRTREE/$NEWFILE
 mv $OSADADIR/dir1 $OSADADIR/mydir
-stat $OSADADIR/mydir/dir2/dir3/$FILE
+stat $OSADADIR/$NEWDIRTREE/$NEWFILE
 
